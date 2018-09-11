c1e282086fa4bbc70e94b3a16b62c512ce7a9314
wheatandcat <wheatandcat@gmail.com>
2018-09-11 15:08:59 +0900

    init

--- native/android/app/src/main/java/com/nativeGraphql/MainApplication.java
+++ native/android/app/src/main/java/com/nativeGraphql/MainApplication.java
@@ -5,6 +5,7 @@ import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -20,10 +21,11 @@ public class MainApplication extends NavigationApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
+          new RNFirebasePackage(),
           new RNGoogleSigninPackage(),
           new RNFirebasePackage(),
-          new RNFirebaseAuthPackage()
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseMessagingPackage()
       );
     }
 
